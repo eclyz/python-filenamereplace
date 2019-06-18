3f33edbe7e23a9a4413365ec005759a20c61081a
eclyz <erdicilliyuz@gmail.com>
2019-06-18 14:20:51 +0300

    Update filenameReplace.py

--- filenameReplace.py
+++ filenameReplace.py
@@ -1,8 +1,8 @@
 import os
 
-for filename in os.listdir("."):
+for filename in os.listdir("."): //current directory
     print(filename)
-    a = filename.replace("jpg", "jpeg")
+    a = filename.replace("jpg", "jpeg") //jpg to jpeg
     os.rename(filename,a)
 
 
